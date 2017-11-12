f87d327969d98609b3ebcd3e49107ba0abc15385
Alexandra Sunderland <alexandra.sunderland@gmail.com>
2017-11-12 14:52:26 -0500

    More setup for deploy

--- settings.py
+++ settings.py
@@ -1,13 +1,28 @@
 import os
+import urlparse
+
+urlparse.uses_netloc.append('mysql')
 
 GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
 
 APP_SECRET_KEY = os.environ['APP_SECRET_KEY']
 
-DATABASE = {
-    'USER': os.environ['MYSQL_USER'],
-    'PWD': os.environ['MYSQL_PWD'],
-    'NAME': os.environ['MYSQL_NAME'],
-    'HOST': 'localhost'
-}
 
+if 'DATABASE_URL' in os.environ:
+    url = urlparse.urlparse(os.environ['DATABASE_URL'])
+    
+    DATABASE = {
+        'USER': url.username,
+        'PWD': url.password,
+        'NAME': url.path[1:],
+        'HOST': url.hostname,
+    }
+else:
+    DATABASE = {
+        'USER': os.environ['MYSQL_USER'],
+        'PWD': os.environ['MYSQL_PWD'],
+        'NAME': os.environ['MYSQL_NAME'],
+        'HOST': os.environ['MYSQL_HOST']
+    }
+
+test = 'mysql://b3b4f631aa3c36:372e5321@eu-cdbr-west-01.cleardb.com/heroku_ed3058ca21821c0?reconnect=true'
