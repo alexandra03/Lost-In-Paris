517ea2790b28c2daff3ee38508de0124fc65fe01
Alexandra Sunderland <alexandra.sunderland@gmail.com>
2017-11-12 15:16:05 -0500

    slight syntax update

--- paris.py
+++ paris.py
@@ -40,7 +40,7 @@ def get_db():
 def init_db():
     db = get_db()
     with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
+        db.cursor().execute(f.read())
     db.commit()
 
 @app.cli.command('initdb')
